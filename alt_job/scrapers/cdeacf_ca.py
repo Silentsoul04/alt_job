e4b6096d1dfa7ee40dfd28c46ff065bb186c674b
tristanlatr <tris.la.tr@gmail.com>
2020-08-21 09:28:03 -0400

    Implement paging on cdeacf.ca scraper

--- alt_job/scrapers/cdeacf_ca.py
+++ alt_job/scrapers/cdeacf_ca.py
@@ -15,5 +15,12 @@ class Scraper_cdeacf_ca(alt_job.scrapers.Scraper):
             'url':urllib.parse.urljoin('http://cdeacf.ca/', selector.xpath('div[contains(@class,"views-field-title")]//a/@href').get()),
             'date_posted':selector.xpath('div[contains(@class,"views-field-created")]//span[@class="field-content-inner"]/text()').get(),
             'organisation':selector.xpath('div[contains(@class,"views-field-field-organisme")]//span[@class="field-content"]/text()').get(),
-            'title':selector.xpath('div[contains(@class,"views-field-title")]//a/text()').get()
+            'title':selector.xpath('div[contains(@class,"views-field-title")]//a/text()').get(),
+            'apply_before': selector.xpath('div[9]/span[2]/span/text()').get(),
+            'location': selector.xpath('div[8]/span/text()').get()
         }
+    
+    def get_next_page_url(self, response):
+        return response.xpath('//*[@id="block-system-main"]/div/div[2]/ul/li[contains(@class,"pager-next")]/a/@href').get()
+
+
