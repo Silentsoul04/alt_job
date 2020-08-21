0f49df2d7ed3d29325bc8e4108cc5dfe8f4d520a
tristanlatr <tris.la.tr@gmail.com>
2020-08-20 20:09:21 -0400

    Add engages.ca scraper

---
+++ alt_job/scrapers/engages_ca.py
@@ -0,0 +1,27 @@
+
+import scrapy
+from bs4 import BeautifulSoup
+import alt_job.scrapers
+from alt_job.jobs import Job
+
+class Scraper_engages_ca(alt_job.scrapers.Scraper):
+    name = "engages.ca"
+    allowed_domains = ["webcache.googleusercontent.com", name]
+
+    def get_jobs_list(self, response):
+        # HTML <section> contains all li of postings
+        return response.xpath('//*[@id="content"]/div/div/div[1]/section/div/article')
+
+    def get_job_dict(self, selector):
+        return {
+            'url':selector.xpath('div/div/h3/a/@href').get(),
+            'title':selector.xpath('div/div/h3/a/text()').get(),
+            'organisation':selector.xpath('div/div/div/a/text()').get(),
+            'location':selector.xpath('div/div/div/text()').get(),
+            'job_type':selector.xpath('div/div/div/span[2]/text()').get()
+        }
+
+    def parse_full_job_page(self, response, job_dict):
+        job_dict['description']=BeautifulSoup(response.xpath('//*[@id="single"]/div[3]').get()).get_text()
+        job_dict['date_posted']=response.xpath('//*[@id="single"]/div[1]/span/b/text()').get()
+        return Job(job_dict)
